a4ac76dc6c98e9e94045ad067a8e4bc64382d1fa
mark <mark@dumay.nl>
2021-02-24 09:28:17 +0100

    Added support to limit runs of each job

--- src/lib/daemon.go
+++ src/lib/daemon.go
@@ -6,24 +6,31 @@ package lib
 import (
 	"os"
 	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/robfig/cron/v3"
 )
 
-// Job defines a single cron job with a cron specification and callback function.
+// Job defines a single cron job with a cron specification and callback function. The Counter tracks the number of time
+// the job has been triggered. The limit defines the maximum number of runs, where 0 means infinite.
 type Job struct {
-	Tag  string
-	Spec string
-	RunE func() error
+	id      cron.EntryID
+	Tag     string
+	Spec    string
+	RunE    func() error
+	Counter int
+	Limit   int
 }
 
 // Result represents a typed goroutine result.
 type Result int
 
-// Defines a pseudo enumeration of possible result types for a goroutine result.
+// Defines a pseudo enumeration of possible result types for a goroutine result. Stopped is triggered when the maximum
+// number of runs for a job has been reached.
 const (
 	Done int = iota
+	Stopped
 	Interrupted
 	Error
 	Fatal
@@ -45,10 +52,16 @@ func worker(jobChan <-chan Job, sigChan <-chan os.Signal, result chan workerResu
 	// wait for an interrupt or new available job; split into two selects to prioritize interrupts over new jobs
 	for {
 		select {
-		case <-sigChan:
-			Logger.Warn().Msg("Worker processing canceled")
+		case sig := <-sigChan:
 			var r workerResult
-			r.result = Result(Interrupted)
+			if sig == syscall.SIGSTOP {
+				Logger.Warn().Msg("Worker processing stopped")
+				r.result = Result(Stopped)
+
+			} else {
+				Logger.Warn().Msg("Worker processing canceled")
+				r.result = Result(Interrupted)
+			}
 			result <- r
 			return
 		default:
@@ -57,6 +70,16 @@ func worker(jobChan <-chan Job, sigChan <-chan os.Signal, result chan workerResu
 		select {
 		case job := <-jobChan:
 			Logger.Debug().Msgf("Worker '%s' started processing new job", job.Tag)
+			if job.Limit > 0 {
+				Logger.Debug().Msgf("Worker '%s' on run %d with limit %d", job.Tag, job.Counter, job.Limit)
+			}
+			if job.Limit > 0 && job.Counter > job.Limit {
+				Logger.Debug().Msgf("Worker '%s' has reached limit of %d runs", job.Tag, job.Limit)
+				var r workerResult
+				r.result = Result(Stopped)
+				result <- r
+				return
+			}
 			if err := job.RunE(); err != nil {
 				Logger.Error().Err(err).Msgf("Could not process worker '%s'", job.Tag)
 				if haltOnError {
@@ -108,7 +131,7 @@ func RunCron(job Job, haltOnError bool) {
 // the callback function cmd of each job to execute a specific command at the defined interval.
 //
 // Jobs run one at a time and are delayed if the previous job is still running. As the cron package does not support
-// chaining across different jobs, each cron job is processed by a single worker routine using a dedicated job channel.
+// chaining across different jobs, all cron job are processed by a single worker routine using a dedicated job channel.
 // Jobs are added to this channel once they are released by the cron scheduler. The channel has a maximum capacity of 5
 // jobs, additional jobs are dropped. The worker routine supports graceful termination.
 func RunCronJobs(jobs []Job, haltOnError bool) error {
@@ -127,19 +150,30 @@ func RunCronJobs(jobs []Job, haltOnError bool) error {
 
 		Logger.Info().Msgf("Scheduling job '%s' with cron spec '%s'", job.Tag, job.Spec)
 		wrapper := func() {
-			// put job in the channel unless it is full
-			select {
-			case jobChan <- job:
-				Logger.Debug().Msgf("Added new job '%s' to channel", job.Tag)
-			default:
-				Logger.Error().Msgf("Dropped job '%s' (channel is full)", job.Tag)
+			job.Counter++
+			// process job if it has not reached it's limit
+			if job.Limit == 0 || job.Counter <= job.Limit {
+				// put job in the channel unless it is full
+				select {
+				case jobChan <- job:
+					Logger.Debug().Msgf("Added new job '%s' to channel", job.Tag)
+				default:
+					Logger.Error().Msgf("Dropped job '%s' (channel is full)", job.Tag)
+				}
+			} else {
+				// remove the job from the scheduler and stop the scheduler when all jobs are done
+				Logger.Debug().Msgf("Stopped job '%s', limit %d is reached", job.Tag, job.Limit)
+				cron.Remove(job.id)
+				if len(cron.Entries()) == 0 {
+					sigChan <- syscall.SIGSTOP
+				}
 			}
-
 		}
 		id, err := cron.AddFunc(job.Spec, wrapper)
 		if err != nil {
 			Logger.Error().Msgf("Could not schedule job '%s'", job.Tag)
 		} else {
+			job.id = id
 			entry := cron.Entry(id)
 			t := entry.Schedule.Next(time.Now()).Format(time.RFC3339)
 			Logger.Info().Msgf("First '%s' job scheduled to run at '%s'", job.Tag, t)
@@ -162,6 +196,9 @@ func RunCronJobs(jobs []Job, haltOnError bool) error {
 	// wait for the worker and terminate on error
 	r := <-result
 	switch r.result {
+	case Result(Stopped):
+		Logger.Info().Msgf("Cron processing stopped")
+		return nil
 	case Result(Interrupted):
 		return &ResticError{"Cron processing interrupted", false}
 	case Result(Error):
