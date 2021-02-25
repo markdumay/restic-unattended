0e9254fc0de0c436de0632d4156aff5c6074f613
mark <mark@dumay.nl>
2021-02-25 17:37:44 +0100

    Refined TestInitLogger()

--- src/lib/logger_test.go
+++ src/lib/logger_test.go
@@ -17,6 +17,93 @@ type LogBuffer []string
 // Private Functions
 //======================================================================================================================
 
+func isValidLevel(t *testing.T, input interface{}, expected zerolog.Level, loggingType string) bool {
+	var level zerolog.Level
+
+	// convert input to typed level, fail on error
+	switch v := input.(type) {
+	case string:
+		s := strings.ToLower(strings.TrimSpace(input.(string)))
+		parsed, err := zerolog.ParseLevel(s)
+		if err != nil {
+			t.Errorf("InitLogger with '%s' formatting could not convert log level", loggingType)
+			return false
+		}
+		level = parsed
+	case zerolog.Level:
+		parsed, ok := input.(zerolog.Level)
+		if !ok {
+			t.Errorf("InitLogger with '%s' formatting could not convert log level", loggingType)
+			return false
+		}
+		level = parsed
+	default:
+		t.Errorf("InitLogger with '%s' formatting could not convert log level of type %T", loggingType, v)
+		return false
+	}
+
+	// validate level equals expected value
+	if level != expected {
+		t.Errorf("InitLogger with '%s' formatting returned incorrect level, got: %s, want: %s.", loggingType,
+			level.String(), expected.String())
+	}
+
+	return true
+}
+
+func isValidMessage(t *testing.T, input string, expected string, loggingType string) bool {
+	s := strings.TrimSpace(input)
+	if s != expected {
+		t.Errorf("InitLogger with '%s' formatting returned incorrect message, got: %s, want: %s.", loggingType,
+			s, expected)
+		return false
+	}
+
+	return true
+}
+
+func isValidTimestamp(t *testing.T, input interface{}, expected time.Time, loggingType string) bool {
+	const layout = "2006-01-02T15:04:05Z07:00"
+	var timestamp time.Time
+
+	// convert input to typed timestamp, fail on error
+	switch v := input.(type) {
+	case string:
+		s, ok := input.(string)
+		if !ok {
+			t.Errorf("InitLogger with '%s' formatting could not convert timestamp string", loggingType)
+			return false
+		}
+		time, err := time.Parse(layout, strings.TrimSpace(s))
+		if err != nil {
+			t.Errorf("InitLogger with '%s' formatting returned incorrect timestamp format, got: %s, want: %s.", "Pretty",
+				timestamp, layout)
+			return false
+		}
+		timestamp = time
+	case time.Time:
+		time, ok := input.(time.Time)
+		if !ok {
+			t.Errorf("InitLogger with '%s' formatting could not convert timestamp", loggingType)
+			return false
+		}
+		timestamp = time
+	default:
+		t.Errorf("InitLogger with '%s' formatting could not convert timestamp of type %T", loggingType, v)
+		return false
+	}
+
+	// validate timestamp is within expected range
+	diff := expected.Sub(timestamp)
+	if diff.Minutes() > 1 {
+		t.Errorf("InitLogger with '%s' formatting returned incorrect timestamp, got: %s, want: %s (+/- 1 min).", "JSON",
+			timestamp, expected.Format(time.RFC3339))
+		return false
+	}
+
+	return true
+}
+
 //======================================================================================================================
 // Public Functions
 //======================================================================================================================
@@ -37,93 +124,66 @@ func TestInitLogger(t *testing.T) {
 		{zerolog.PanicLevel, "Panic message"},
 	}
 
-	// test Default logging for levels >= Error
+	// test Default logs for levels >= Error conforms to template "ERROR  Error message"
 	InitLoggerWithWriter(LogFormat(Default), &buffer, true)
 	zerolog.SetGlobalLevel(zerolog.ErrorLevel)
 	buffer = LogBuffer{}
 	for _, item := range table {
-		// validate each log message conforms to template "ERROR  Error message"
+		// generate and capture a log message
 		Logger.WithLevel(item.level).Msg(item.message)
-		if item.level >= zerolog.ErrorLevel {
-			log := buffer[len(buffer)-1]
-			level := strings.ToUpper(item.level.String())
-			msg := strings.TrimSpace(strings.TrimPrefix(log, level))
-
-			if msg != item.message {
-				t.Errorf("InitLogger with '%s' formatting returned incorrect log message, got: '%s', want: '%s'.", "Default",
-					msg, item.message)
-			}
+		if item.level < zerolog.ErrorLevel {
+			continue
 		}
+		log := buffer[len(buffer)-1]
+
+		// validate level and message (level is tested implicitly)
+		level := strings.ToUpper(item.level.String())
+		msg := strings.TrimPrefix(log, level)
+		isValidMessage(t, msg, item.message, "Default")
 	}
 
-	// test Pretty logging for all levels
+	// test Pretty logs for all levels conforms to template "2021-01-01T00:00:00Z | TRACE  | Trace message"
 	InitLoggerWithWriter(LogFormat(Pretty), &buffer, true)
 	zerolog.SetGlobalLevel(zerolog.TraceLevel)
 	buffer = LogBuffer{}
 	for _, item := range table {
-		// validate each log message conforms to template "2021-01-01T00:00:00Z | TRACE  | Trace message"
+		// generate and capture a log message
 		Logger.WithLevel(item.level).Msg(item.message)
 		log := buffer[len(buffer)-1]
 		elements := strings.Split(string(log), "|")
 		if len(elements) != 3 {
 			t.Errorf("InitLogger with '%s' formatting returned incorrect number of elements, got: %d, want: %d.",
 				"Pretty", len(elements), 3)
-		} else {
-			timestamp := strings.TrimSpace(elements[0])
-			level := strings.TrimSpace(elements[1])
-			msg := strings.TrimSpace(elements[2])
-			// validate first element is a timestamp
-			if _, err := time.Parse(layout, timestamp); err != nil {
-				t.Errorf("InitLogger with '%s' formatting returned incorrect timestamp format, got: %s, want: %s.", "Pretty",
-					timestamp, layout)
-			}
-			// validate second element has correct logging level
-			if level != strings.ToUpper(item.level.String()) {
-				t.Errorf("InitLogger with '%s' formatting returned incorrect level, got: %s, want: %s.", "Pretty",
-					level, strings.ToUpper(item.level.String()))
-			}
-			// validate third element has expected log message
-			if msg != item.message {
-				t.Errorf("InitLogger with '%s' formatting returned incorrect message, got: %s, want: %s.", "Pretty",
-					msg, item.message)
-			}
+			continue
 		}
+
+		// validate timestamp, level, and message
+		isValidTimestamp(t, elements[0], time.Now(), "Pretty")
+		isValidLevel(t, elements[1], item.level, "Pretty")
+		isValidMessage(t, elements[2], item.message, "Pretty")
 	}
 
-	// test JSON logging for all levels
+	// test JSON logs for all levels
 	InitLoggerWithWriter(LogFormat(JSON), &buffer, true)
 	zerolog.SetGlobalLevel(zerolog.TraceLevel)
 	buffer = LogBuffer{}
 	for _, item := range table {
-		// validate each log message conforms to expected JSON response
+		// generate and capture a log message
 		Logger.WithLevel(item.level).Msg(item.message)
 		log := buffer[len(buffer)-1]
-		// validate JSON response string can be unmarshalled
+
+		// validate that JSON response string can be unmarshalled
 		jsonLog, err := UnmarshalLog([]byte(log))
 		if err != nil {
-			// if err := json.Unmarshal([]byte(log), &jsonLog); err != nil {
 			t.Errorf("InitLogger with '%s' formatting returned incorrect log message, error: '%s'.", "JSON",
 				err.Error())
 			continue
 		}
 
-		// validate first element is a timestamp and within expected range
-		currentTime := time.Now()
-		diff := currentTime.Sub(jsonLog.Time)
-		if diff.Minutes() > 1 {
-			t.Errorf("InitLogger with '%s' formatting returned incorrect timestamp, got: %s, want: %s (+/- 1 min).", "JSON",
-				jsonLog.Time.Format(layout), currentTime.Format(layout))
-		}
-		// validate logging level
-		if jsonLog.Level != item.level {
-			t.Errorf("InitLogger with '%s' formatting returned incorrect level, got: %d, want: %d (%s).", "JSON",
-				jsonLog.Level, item.level, strings.ToUpper(item.level.String()))
-		}
-		// validate log message
-		if jsonLog.Message != item.message {
-			t.Errorf("InitLogger with '%s' formatting returned incorrect message, got: %s, want: %s.", "Pretty",
-				jsonLog.Message, item.message)
-		}
+		// validate timestamp, level, and message
+		isValidTimestamp(t, jsonLog.Time, time.Now(), "Pretty")
+		isValidLevel(t, jsonLog.Level, item.level, "Pretty")
+		isValidMessage(t, jsonLog.Message, item.message, "Pretty")
 	}
 }
 
