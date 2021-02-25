13baac2c26fa4ee506db2eeb5c454643e193a5fe
mark <mark@dumay.nl>
2021-02-25 20:06:45 +0100

    Added TestInitSecrets()

---
+++ src/lib/secrets_test.go
@@ -0,0 +1,49 @@
+// Copyright © 2021 Mark Dumay. All rights reserved.
+// Use of this source code is governed by The MIT License (MIT) that can be found in the LICENSE file.
+
+package lib
+
+import (
+	"path"
+	"strings"
+	"testing"
+)
+
+//======================================================================================================================
+// Public Functions
+//======================================================================================================================
+
+func TestInitSecrets(t *testing.T) {
+	// create some file-based secrets and env variables for testing
+	secrets := GetSupportedSecrets()
+	env := map[string]string{}
+	for secret := range secrets {
+		name := strings.TrimSuffix(secret, "_FILE")
+		path := path.Join(t.TempDir(), name)
+		WriteLine(path, name)
+		env[secret] = path
+	}
+
+	// read the test secrets from files
+	vars, err := InitSecrets(env)
+	if err != nil {
+		t.Errorf("InitSecrets returned an error: %s.", err.Error())
+		return
+	}
+
+	// validate all secrets are accounted for
+	if len(vars) != len(secrets) {
+		t.Errorf("InitSecrets returned incorrect number of variables, got: %d, want: %d.",
+			len(vars), len(secrets))
+		return
+	}
+
+	// validate the contents of each individual secret; left of '=' sign should equal right of '=' sign
+	for _, v := range vars {
+		pair := strings.SplitN(v, "=", 2)
+		if pair[0] != pair[1] {
+			t.Errorf("InitSecrets returned an incorrect secret, got: %s, want: %s.",
+				pair[0], pair[1])
+		}
+	}
+}
